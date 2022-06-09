cdf6e22d281a9c5e4f66967fd602138d60bbca03
Elithon Silva <elithonsilva@gmail.com>
2022-06-09 17:48:33 -0300

    implementa teste para o construtor/inicializador do objeto Produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    new_product = Product(
+        1,
+        "teste01",
+        "empresa01",
+        "09/06/2022",
+        "09/06/2023",
+        "KLRDB62QZP",
+        "Armazenar em local núvem",
+    )
+    assert new_product.id == 1
+    assert new_product.nome_do_produto == "teste01"
+    assert new_product.nome_da_empresa == "empresa01"
+    assert new_product.data_de_fabricacao == "09/06/2022"
+    assert new_product.data_de_validade == "09/06/2023"
+    assert new_product.numero_de_serie == "KLRDB62QZP"
+    assert new_product.instrucoes_de_armazenamento == "Armazenar em local núvem"
