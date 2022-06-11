593fdb61772d0ca38fd3d11ff34164c35885fe06
Elithon Silva <elithonsilva@gmail.com>
2022-06-11 13:06:39 -0300

    implementa teste para o relatório individual do produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,25 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
+
+def data_products(param):
+    return (
+        f"O produto {param.nome_do_produto}"
+        f" fabricado em {param.data_de_fabricacao}"
+        f" por {param.nome_da_empresa} com validade"
+        f" até {param.data_de_validade}"
+        f" precisa ser armazenado {param.instrucoes_de_armazenamento}."
+    )
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "teste01",
+        "empresa01",
+        "09/06/2022",
+        "09/06/2023",
+        "KLRDB62QZP",
+        "Armazenar em nuvem",
+    )
+
+    assert str(product) == data_products(product)
