b0ce34cca13e228121fa1d04089cbd8ee21f54bb
Elithon Silva <elithonsilva@gmail.com>
2022-06-22 23:50:09 -0300

    implementa metodo arquivo_json para gerar os relatórios através de um arquivo JSON

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -14,16 +15,26 @@ class leitura_arquivo:
         arquivo.close()
         return data
 
+    @classmethod
+    def arquivo_json(cls, path):
+        with open(path) as arquivo:
+            data = json.load(arquivo)
+        return data
+
 
 class Inventory:
 
     list
 
     @classmethod
-    def import_data(cls, path, report_type):
+    def import_data(cls, path, report):
         if ".csv" in path:
             cls.list = leitura_arquivo.arquivo_csv(path)
-        if report_type == "simples":
+
+        if ".json" in path:
+            cls.list = leitura_arquivo.arquivo_json(path)
+
+        if report == "simples":
             return SimpleReport.generate(cls.list)
         else:
             return CompleteReport.generate(cls.list)
